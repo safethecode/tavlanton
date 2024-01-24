fa2768f9e191316b4e8199f184ac542f3e8ec2da
safethecode <iam@sonjim.in>
2024-01-24 17:06:59 +0900

    변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,14 @@ import { NextRequest, NextResponse } from 'next/server';
 function authenticationMiddleware(request: NextRequest) {
   if (!(request.headers as any).cookie?.includes('authenticated=true')) {
     if (!request.url.includes('/auth')) {
-      NextResponse.redirect(new URL('/auth', request.url));
-      return NextResponse.next();
+      return NextResponse.redirect(new URL('/auth', request.url));
     }
   }
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
+};
 
 export default authenticationMiddleware;
