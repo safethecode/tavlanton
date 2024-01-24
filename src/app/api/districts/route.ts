28f3c810eacf922d3c6197e67ad2855a4c7bf209
safethecode <iam@sonjim.in>
2024-01-24 10:51:25 +0900

    asd

--- src/app/api/districts/route.ts
+++ src/app/api/districts/route.ts
@@ -2,5 +2,5 @@ import { supabaseServer } from '@/utils';
 
 export async function GET() {
   const districts = await supabaseServer.from('districts').select('*');
-  return Response.json(districts);
+  return (Response as any).json(districts);
 }
