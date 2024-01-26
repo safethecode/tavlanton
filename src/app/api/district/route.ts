f1d8e895a5e724c17239541560169abda1327a9c
safethecode <iam@sonjim.in>
2024-01-26 13:52:56 +0900

    구역 조회 api 추가

---
+++ src/app/api/district/route.ts
@@ -0,0 +1,31 @@
+import { supabaseServer } from '@/utils/api';
+import { NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const districtId = req.url.includes('?district_id=')
+    ? req.url.split('?district_id=')[1]
+    : '';
+
+  const districtInUsers = await supabaseServer
+    .from('users')
+    .select('*')
+    .eq('districts_id', districtId);
+
+  const districtInPoints = await supabaseServer
+    .from('users')
+    .select('*')
+    .eq('districts_id', districtId)
+    .select('point');
+
+  const pointsSum = districtInPoints.data!.reduce(
+    (sum, point) => sum + point.point,
+    0,
+  );
+
+  return (Response as any).json({
+    status: 200,
+    users: districtInUsers,
+    points: pointsSum,
+    length: districtInUsers.data!.length,
+  });
+}
