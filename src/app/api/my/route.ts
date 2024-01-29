36e0569de08d0f13b266bf9086d5addef3c35b97
safethecode <iam@sonjim.in>
2024-01-29 20:59:00 +0900

    id 기반 my api 추가

---
+++ src/app/api/my/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from 'next/server';
+import { supabaseServer } from '@/utils';
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+
+  const userId = searchParams.get('user_id');
+
+  const { data } = await supabaseServer
+    .from('users')
+    .select('*')
+    .eq('id', userId)
+    .single();
+
+  if (data) {
+    return Response.json(data);
+  }
+}
