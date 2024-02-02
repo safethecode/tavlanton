60a5eacabad74b97013bcb68b674c05ad039ec8b
safethecode <iam@sonjim.in>
2024-02-02 22:30:49 +0900

    너무 느려

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,42 +9,42 @@ async function authenticationMiddleware(request: NextRequest) {
     }
   }
   if (authenticated?.value === 'true') {
-    const user = request.cookies.get('user');
+    // const user = request.cookies.get('user');
 
     if (request.url.includes('/auth')) {
       return NextResponse.redirect(new URL('/', request.url));
     }
 
-    if (user?.value && !request.url.includes('/api/my')) {
-      try {
-        const userId = JSON.parse(user?.value).id;
+    // if (user?.value && !request.url.includes('/api/my')) {
+    //   try {
+    //     const userId = JSON.parse(user?.value).id;
 
-        const URL = new URLSearchParams({
-          user_id: userId,
-        }).toString();
+    //     const URL = new URLSearchParams({
+    //       user_id: userId,
+    //     }).toString();
 
-        const res = await fetch('https://대청비전.com/api/my' + '?' + URL, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+    //     const res = await fetch('https://대청비전.com/api/my' + '?' + URL, {
+    //       method: 'GET',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     });
 
-        const data = await res.json();
+    //     const data = await res.json();
 
-        const response = NextResponse.next();
+    //     const response = NextResponse.next();
 
-        response.cookies.set('user', JSON.stringify(data), {
-          maxAge: 60 * 60 * 24 * 365,
-        });
+    //     response.cookies.set('user', JSON.stringify(data), {
+    //       maxAge: 60 * 60 * 24 * 365,
+    //     });
 
-        return response;
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-        // Handle the error here, e.g. return an error response
-        return new Response('Error parsing JSON', { status: 500 });
-      }
-    }
+    //     return response;
+    //   } catch (error) {
+    //     console.error('Error parsing JSON:', error);
+    //     // Handle the error here, e.g. return an error response
+    //     return new Response('Error parsing JSON', { status: 500 });
+    //   }
+    // }
   }
 }
 
