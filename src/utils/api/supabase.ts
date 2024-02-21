becd1c61f8e4d49fb66d07d965dd3d0dc55d0f0d
safethecode <iam@sonjim.in>
2024-02-21 12:49:48 +0900

    feat(utils/api): create-fetch 함수로 no-store 적용

--- src/utils/api/supabase.ts
+++ src/utils/api/supabase.ts
@@ -1,7 +1,16 @@
 'use client';
 import { createBrowserClient } from '@supabase/ssr';
+import { createFetch } from './createFetch';
 
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    cookies: {},
+    global: {
+      fetch: createFetch({
+        cache: 'no-store',
+      }),
+    },
+  },
 );
--- src/utils/api/supabaseSsr.ts
+++ src/utils/api/supabaseSsr.ts
@@ -1,9 +1,15 @@
 import { createServerClient } from '@supabase/ssr';
+import { createFetch } from './createFetch';
 
 export const supabaseServer = createServerClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     cookies: {},
+    global: {
+      fetch: createFetch({
+        cache: 'no-store',
+      }),
+    },
   },
 );
